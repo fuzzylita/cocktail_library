a3a3769eae7ca0116cb605e15aa8d3a3a11fa43f
fuzzylita <lita.stephenson@gmail.com>
2017-09-25 11:21:18 -0700

    small code improvements

--- lib/cocktail_library/cli.rb
+++ lib/cocktail_library/cli.rb
@@ -1,58 +1,58 @@
 
 class CocktailLibrary::CLI
-  
+
   def initialize
     @bases = CocktailLibrary::BASES
     @cocktail_db = CocktailLibrary::CocktailDB.new
-  end 
+  end
 
   def run
     greeting
-    base_selection 
+    base_selection
     drinks_available
     drink_directions
   end
 
   def greeting
-  puts <<-DOC              
+  puts <<-DOC
 
 
-                            /|     /|                            
-                          // ( `""-                             
-                        \\| /      .\\___                         
-                        /        .      4                        
-                       /              _ /                        
-                      /            .--'                          
-                     (              \\                          
-                    /               \\ \\                        
-                   /            \\ \\  \\                       
-        ,         /              \\   \\ \\                       
-        )\\      /                \\   \\                        
-      \\/  |   .'`   _                \\           \\______/_/        
-    \\.   /  .'       '.        ,\\ \\/              \\   (o)/         
-  \\'   /   /          \\  ;    |   /                \\  ' /          
-  \\'   \\  |           |  |    |  '                  \\  /           
-    \\,   `" |           /.|   | |                    ||            
-        ''-..-\\      _.;.'|   |.;-.                  ||            
-              \\    <`.._ )) |  .;-.))               / \\           
-              (__.  `  ))-' \\_    ))'              +++++          
+                            /|     /|
+                          // ( `""-
+                        \\| /      .\\___
+                        /        .      4
+                       /              _ /
+                      /            .--'
+                     (              \\
+                    /               \\ \\
+                   /            \\ \\  \\
+        ,         /              \\   \\ \\
+        )\\      /                \\   \\
+      \\/  |   .'`   _                \\           \\______/_/
+    \\.   /  .'       '.        ,\\ \\/              \\   (o)/
+  \\'   /   /          \\  ;    |   /                \\  ' /
+  \\'   \\  |           |  |    |  '                  \\  /
+    \\,   `" |           /.|   | |                    ||
+        ''-..-\\      _.;.'|   |.;-.                  ||
+              \\    <`.._ )) |  .;-.))               / \\
+              (__.  `  ))-' \\_    ))'              +++++
                    `'--"`      `"""`
   DOC
 
   puts <<~DOC
     Hello there, I am Tiki, your very floofy bartender!
     I'm going to give you a list of drinks based on whatever liquor you have on hand.
-    
+
     What would you like to use for your drink?
 
     DOC
-  end 
+  end
 
   def base_selection
     @bases.each_with_index do |base, index|
       puts "#{index + 1}. #{base}"
-    end 
-    
+    end
+
     puts ""
     puts "Please enter the number of the base you'd like to use, or enter exit to quit."
 
@@ -60,36 +60,39 @@ class CocktailLibrary::CLI
     @base_choice = gets.chomp.downcase
     if @base_choice == "exit"
       puts ""
-      puts ""
+
       puts "Perhaps the smoothie place down the road would be more to your liking."
       exit
-    elsif !@bases.include?(@bases[@base_choice.to_i-1]) || @base_choice.to_i < 1
+    elsif @bases[@base_choice.to_i-1] == nil || @base_choice.to_i < 1
+
+      puts ""
       puts "Ehmm... Our menu is a little limited.. sorry."
       puts "Do you have anything on hand from these options?"
+      puts ""
       base_selection
     else
       #this is defining the search object
       @base_type = @bases[@base_choice.to_i-1].downcase
-    end 
+    end
   end
 
   def drinks_available
     #this calls the search
     @drinks_list = @cocktail_db.search_by_base(@base_type)
-    
-    #escape for incorrect entry
+
+    #escape for empty return
     if @drinks_list == nil
       puts "I'm sorry, we have no idea how to make drinks with #{@base_type.gsub(/\w+/) {|word| word.capitalize}}."
       puts "Here is a beer."
       exit(0)
     else
       puts "Here are the drinks you can make with #{@base_type.gsub(/\w+/) {|word| word.capitalize}}:"
-      
-      puts @drinks_list
-    end 
-  end 
 
-  def drink_directions 
+      puts @drinks_list
+    end
+  end
+
+  def drink_directions
     puts ""
     puts "Which of these would you like to make?"
     puts ""
@@ -111,24 +114,24 @@ class CocktailLibrary::CLI
       puts "Hmm, not sure how to make that one... Sorry about that!"
       puts ""
       drink_directions
-      
-    else 
+
+    else
       #take drink selection and push it into the API
       drink = @cocktail_db.drink_components(drink_selection)
-      
+
       puts "One second, let me grab that recipe for you!"
       sleep(1)
 
       puts ""
       puts "Ok, here how to make your #{drink.name.gsub(/\w+/) {|word| word.capitalize}}:  "
-      
+
       drink.ingredients.each_with_index do |ing, idx|
         puts "#{drink.measurements[idx]}: #{ing}"
-      end 
-      
+      end
+
       puts "#{drink.instructions}"
       puts ""
       puts "Hope you enjoy it!"
-    end 
+    end
   end
 end
--- lib/cocktail_library/cocktail_db.rb
+++ lib/cocktail_library/cocktail_db.rb
@@ -7,32 +7,32 @@ class CocktailLibrary::CocktailDB
     base_results = open("http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{base}")
 
     #this code breaks that object (StringIO) into a parsable hash
-    #and then it returns a list of the names of the drinks 
+    #and then it returns a list of the names of the drinks
     drink_names = []
 
     JSON.load(base_results)['drinks'].each do |k,v|
       k.each do |k, v|
         if k == "strDrink"
           drink_names << v
-        end 
+        end
       end
     end
     drink_names
-  end 
+  end
 
   def drink_components(drink)
     drink_results = open("http://www.thecocktaildb.com/api/json/v1/1/search.php?s=#{drink}")
 
-    drink_details = JSON.load(drink_results)["drinks"].first.reject do |k,v|
+    drink_details = JSON.load(drink_results)['drinks'].first.reject do |k,v|
       v == "" || v == " " || v == nil
     end
-    
+
     instructions = drink_details["strInstructions"]
     ingredients = []
     measurements = []
-    
+
     drink_details.each do |k,v|
-      #the API returns the ingredients and measurements ordered to match the other, so we can be confident that 
+      #the API returns the ingredients and measurements ordered to match the other, so we can be confident that
       #when we add them to an array in order, they will be returned properly
       if k.include?("Ingredient")
         ingredients << v
@@ -43,5 +43,5 @@ class CocktailLibrary::CocktailDB
       end
     end
     CocktailLibrary::Drink.new(drink, instructions, ingredients, measurements)
-  end 
-end 
+  end
+end
